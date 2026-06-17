1d256afc59eb5e5efb71f00a21db6ae54bbba870
Harsh Gondaliya <harsh.qa.moweb@gmail.com>
2026-06-17 16:10:32 +0530

    Edited conftest.y

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
+import os
 import pytest
 from playwright.sync_api import Playwright
 
 @pytest.fixture
 def page(playwright:Playwright):
-    browser = playwright.chromium.launch(headless=False,args=["--start-maximized"])
+    headless = os.getenv("HEADLESS", "False").lower() == "true"
+    browser = playwright.chromium.launch(headless=headless,args=["--start-maximized"])
     context = browser.new_context(no_viewport=True)
     page = context.new_page()
     # page.set_viewport_size({"width": 1536, "height": 816})
