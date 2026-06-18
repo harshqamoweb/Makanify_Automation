487287b2d4f1f1daf3fd7d3200d1e949a1939e7d
Harsh Gondaliya <harsh.qa.moweb@gmail.com>
2026-06-18 17:55:43 +0530

    Removed test suite

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright
 
 @pytest.fixture
 def page(playwright:Playwright):
-    headless = os.getenv("HEADLESS", "True").lower() == "true"
+    headless = os.getenv("HEADLESS", "False").lower() == "true"
     browser = playwright.chromium.launch(headless=headless,args=["--start-maximized"])
     context = browser.new_context(no_viewport=True)
     page = context.new_page()
